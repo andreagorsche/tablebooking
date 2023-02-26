16480f0c026f84bea6e308ab63be1b93818e3831
andreagorsche <andrea.gorsche@gmail.com>
2023-02-26 22:11:44 +0000

    Add listdisplay, search fields and list filters to the three models on the admin page

--- tablebooking/admin.py
+++ tablebooking/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
-from .models import Table
-from .models import Reservation
-from .models import WaitingList
+from .models import Table, Reservation, WaitingList
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Reservation)
-class PostAdmin(SummernoteModelAdmin):
+class ReservationAdmin(SummernoteModelAdmin):
+    
+    list_display = ('table', 'date', 'guest_name', 'number_of_people')
+    search_fields = ['table', 'guest_name']
+    list_filter = ('table','date','number_of_people')
     summernote_fields = ('comment')
 
-# Register your models here.
-admin.site.register(Table)
-admin.site.register(WaitingList)
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('table_no', 'number_of_people', 'private_booth')
+    search_fields = ['table_no', 'number_of_people', 'private_booth']
+    list_filter = ('table_no', 'number_of_people', 'private_booth')   
+
+@admin.register(WaitingList)
+class WaitingListAdmin(admin.ModelAdmin):
+    list_display = ('table', 'rank', 'reservation')
+    search_fields = ['table', 'rank', 'reservation']
+    list_filter = ('table', 'rank', 'reservation')  
+
 
