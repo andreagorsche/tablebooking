7794da8f08e12e21779647658f317a1531273dce
andreagorsche <andrea.gorsche@gmail.com>
2023-02-26 21:51:40 +0000

    Install Summernote and use it for styling the comment field in the reservations panel

--- tablebooking/admin.py
+++ tablebooking/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from .models import Table
 from .models import Reservation
 from .models import WaitingList
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Reservation)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('comment')
 
 # Register your models here.
-admin.site.register(Reservation)
 admin.site.register(Table)
 admin.site.register(WaitingList)
 
