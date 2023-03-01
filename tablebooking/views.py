7fdfa78ab98eea7548d54521ff17d908457dd9c3
andreagorsche <andrea.gorsche@gmail.com>
2023-03-01 20:16:27 +0000

    Complete class ReservationList view

--- tablebooking/views.py
+++ tablebooking/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Reservation
+from .models import Reservation, Guest
 
 class ReservationList (generic.ListView):
     model = Reservation
-    queryset = Reservation.object.filter(Guest)
+    queryset = Reservation.object.filter(username)order_by('-date')
+    template_name = "manage_reservations.html"
+    paginate_by = 5
