09935f6faa569a9f8d19bee65c285d2573c5bf14
andreagorsche <andrea.gorsche@gmail.com>
2023-03-03 15:54:24 +0000

    Add menu.html, delete register and login page

--- tablebooking/urls.py
+++ tablebooking/urls.py
@@ -2,10 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.base)
-    path('booking_form/', views.booking_form)
-    path('login/', views.login)
-    path('manage_booking/', views.manage_booking)
-    path('register/', views.register)
-
+    path('', views.base),
+    path('booking_form/', views.booking_form),
+    path('menu/', views.login),
+    path('manage_booking/', views.manage_booking),
 ]
--- tablebooking/views.py
+++ tablebooking/views.py
@@ -7,10 +7,7 @@ def booking_form(request):
     return render(request, 'tablebooking/booking_form.html')
 
 def login(request):
-    return render(request, 'tablebooking/login.html')
+    return render(request, 'tablebooking/menu.html')
 
 def manage_booking(request):
     return render(request, 'tablebooking/manage_booking.html')
-
-def registerg(request):
-    return render(request, 'tablebooking/register.html')
