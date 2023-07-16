a042429eb09e3921e8166a2cfce9de2e69d70791
andreagorsche <andrea.gorsche@gmail.com>
2023-07-16 17:42:24 +0000

    Add admin filter is_waitlisted so the admin can see which reservations are on the waiting list

--- tablebooking/admin.py
+++ tablebooking/admin.py
@@ -5,9 +5,9 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Reservation)
 class ReservationAdmin(SummernoteModelAdmin):
     
-    list_display = ('table', 'date', 'number_of_guests')
+    list_display = ('table', 'date', 'number_of_guests', 'is_waitlisted')
     search_fields = ['table']
-    list_filter = ('table','date','number_of_guests')
+    list_filter = ('table','date','number_of_guests', 'is_waitlisted')
     summernote_fields = ('comment')
 
 @admin.register(Table)
