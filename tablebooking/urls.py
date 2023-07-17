c25eab6194c402989d16ac2d12011040944024e9
andreagorsche <andrea.gorsche@gmail.com>
2023-07-17 11:53:32 +0000

    Create the ReservationUpdate view and wire it up with the urls.py file

--- tablebooking/urls.py
+++ tablebooking/urls.py
@@ -1,11 +1,12 @@
 from . import views
 from django.urls import path
-from .views import CreateReservation, ReservationList
+from .views import CreateReservation, ReservationList, ReservationUpdate
 
 urlpatterns = [
     path('', views.base, name='home'),
 #   path('booking_form/', views.booking_form, name='booking_form'),
     path('create_booking/', CreateReservation.as_view(), name='create_reservation'),
     path('list_booking/', ReservationList.as_view(), name='list_reservation'),
+    path('manage_booking/<int:pk>/', ReservationUpdate.as_view(), name='manage_reservation'),
     path('menu/', views.login, name='menu'),
 ]
--- tablebooking/views.py
+++ tablebooking/views.py
@@ -11,6 +11,8 @@ from .forms import ReservationForm
 def base(request):
     return render(request, 'tablebooking/base.html')
 
+def login(request):
+    return render(request, 'tablebooking/menu.html')
 
 class CreateReservation(CreateView):
     model = Reservation
@@ -25,12 +27,14 @@ class CreateReservation(CreateView):
             form.instance.table = tables.first()
         return super().form_valid(form)
 
-def login(request):
-    return render(request, 'tablebooking/menu.html')
-
 class ReservationList(generic.ListView):
     model = Reservation
     queryset = Reservation.objects.all()
     template_name = "tablebooking/list_booking.html"
     paginate_by = 5
 
+class ReservationUpdate (generic.UpdateView):
+    model = Reservation
+    fields = ('date', 'time', 'number_of_guests', 'number_of_child_seats', 'comment')
+    template_name = "tablebooking/manage_booking.html"
+    success_url = reverse_lazy('list_reservation') 
