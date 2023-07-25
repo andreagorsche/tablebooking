01b9d5bcd9af446aaeef106a1a6319f67ce8cefd
andreagorsche <andrea.gorsche@gmail.com>
2023-07-25 11:36:14 +0000

    Create clean method to check date format dd/mm/yyyy and use arrow.utcnow to check if date of booking is not in the past

--- tablebooking/forms.py
+++ tablebooking/forms.py
@@ -1,15 +1,35 @@
 from django import forms
 from .models import Reservation, Table
 from datetime import datetime
+import arrow
+from django.core.exceptions import ValidationError
 
 class ReservationForm(forms.ModelForm):
     date = forms.DateField(input_formats=['%d/%m/%Y'])  # Set the input format
+
+    def clean(self):
+        cleaned_data = super(ReservationForm, self).clean()
+        date = cleaned_data.get('date')
+
+        # Check if the date is in the correct format: dd/mm/yyyy
+        try:
+            datetime.strptime(str(date), '%Y-%m-%d')
+        except ValueError:
+            raise forms.ValidationError('Invalid date format. Please use the format DD/MM/YYYY.')
+
+        # Check if the date is in the past
+        if date and arrow.utcnow().datetime < date:
+            raise forms.ValidationError('You can’t book a table in the past')
+        
+        return cleaned_data
 
     class Meta:
         model = Reservation
         fields = ('date', 'time', 'number_of_guests', 'number_of_child_seats', 'comment')
 
-    # Attempt to parse the date string into a datetime object
+
+
+    ''' Attempt to parse the date string into a datetime object
     def clean_date(self):
         date = self.cleaned_data['date']
         try:
@@ -17,4 +37,4 @@ class ReservationForm(forms.ModelForm):
         except ValueError:
             raise forms.ValidationError("This date is not in the right format. Please use DD/MM/YYYY")
             print("error")
-        return date
+        return date'''
