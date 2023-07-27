3ed4d6b627b65fd1602041bbbcfca9e2f5a7297f
andreagorsche <andrea.gorsche@gmail.com>
2023-07-27 14:14:54 +0000

    Adapt the forms validation with the clean method according to the advice from Roman Rakic in slack

--- tablebooking/forms.py
+++ tablebooking/forms.py
@@ -5,41 +5,34 @@ import arrow
 from django.core.exceptions import ValidationError
 
 class ReservationForm(forms.ModelForm):
-    date = forms.DateField(input_formats=['%d/%m/%Y'])  # Set the input format
-
-    def clean(self):
-        cleaned_data = super(ReservationForm, self).clean()
-        date = cleaned_data.get('date')
-        number_of_guests = cleaned_data.get('number_of_guests')
-
-        # Check if the date is in the correct format: dd/mm/yyyy
-        try:
-            datetime.strptime(str(date), '%Y-%m-%d')
-        except ValueError:
-            raise forms.ValidationError('Invalid date format. Please use the format DD/MM/YYYY.')
-        
-        # Check if the date is in the past
-        if date and arrow.utcnow().datetime < date:
-            raise forms.ValidationError('You can’t book a table in the past')
-        
-        #Check if number_of_guests are not None and not small or equal to null
-        if number_of_guests is not None and number_of_guests <= 0:
-            raise forms.ValidationError('Number of guests must be greater than 0.')
-
-        return cleaned_data
-
+    # Check if the date is in the correct format: dd/mm/yyyy
+    date = forms.DateField(
+        input_formats=["%d/%m/%Y"],
+        error_messages={
+            "invalid": "Please enter a valid date in the format DD/MM/YYYY."
+        },
+    )
+    # Check if the time is in the correct format: hh:mm
+    time = forms.TimeField(
+        error_messages={"invalid": "Please enter a valid time in the format HH:MM."}
+    )
     class Meta:
         model = Reservation
-        fields = ('date', 'time', 'number_of_guests', 'number_of_child_seats', 'comment')
-
-
-
-    ''' Attempt to parse the date string into a datetime object
-    def clean_date(self):
-        date = self.cleaned_data['date']
-        try:
-            datetime.strptime(date, '%d/%m/%Y')
-        except ValueError:
-            raise forms.ValidationError("This date is not in the right format. Please use DD/MM/YYYY")
-            print("error")
-        return date'''
+        fields = (
+            "date",
+            "time",
+            "number_of_guests",
+            "number_of_child_seats",
+            "comment",
+        )
+    def clean(self):
+        cleaned_data = super(ReservationForm, self).clean()
+        date = cleaned_data.get("date")
+        number_of_guests = cleaned_data.get("number_of_guests")
+        # Check if the date is in the past
+        if date and date < date.today():
+            raise forms.ValidationError("You can't book a table in the past")
+        # Check if number_of_guests are not None and not small or equal to null
+        if number_of_guests is not None and number_of_guests <= 0:
+            raise forms.ValidationError("Number of guests must be greater than 0.")
+        return cleaned_data
