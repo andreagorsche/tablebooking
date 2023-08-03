071384a31f6b0d962ae196e28c1a0b3ffce16e67
andreagorsche <andrea.gorsche@gmail.com>
2023-08-03 14:50:31 +0000

    Debug set to False for testing the error pages

--- deliciousdaily/settings.py
+++ deliciousdaily/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['deliciousdaily.herokuapp.com', '8000-andreagorsc-tablebookin-2qz7nfl997o.ws-eu102.gitpod.io']
 
